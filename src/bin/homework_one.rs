34423b95415e131f3e61d3ef4a9b32a71185b038
James Marcil <james@jamesmarcil.com>
2023-03-18 11:06:12 -0400

    Implementation of direct address.

--- src/bin/homework_one.rs
+++ src/bin/homework_one.rs
@@ -57,59 +57,64 @@ const MOD_RM_NO_DISP: u8 = 0b11;
 //------------------------------
 // 1 | 1 | 1 |   BH   |   DI   |
 //------------------------------
-fn get_reg(reg: u8, is_word: bool) -> &'static str {
+fn get_reg(reg: u8, is_word: bool) -> String {
     if is_word {
         match reg {
-            0b000 => "AX",
-            0b001 => "CX",
-            0b010 => "DX",
-            0b011 => "BX",
-            0b100 => "SP",
-            0b101 => "BP",
-            0b110 => "SI",
-            0b111 => "DI",
+            0b000 => String::from("AX"),
+            0b001 => String::from("CX"),
+            0b010 => String::from("DX"),
+            0b011 => String::from("BX"),
+            0b100 => String::from("SP"),
+            0b101 => String::from("BP"),
+            0b110 => String::from("SI"),
+            0b111 => String::from("DI"),
             _ => unreachable!(),
         }
     } else {
         match reg {
-            0b000 => "AL",
-            0b001 => "CL",
-            0b010 => "DL",
-            0b011 => "BL",
-            0b100 => "AH",
-            0b101 => "CH",
-            0b110 => "DH",
-            0b111 => "BH",
+            0b000 => String::from("AL"),
+            0b001 => String::from("CL"),
+            0b010 => String::from("DL"),
+            0b011 => String::from("BL"),
+            0b100 => String::from("AH"),
+            0b101 => String::from("CH"),
+            0b110 => String::from("DH"),
+            0b111 => String::from("BH"),
             _ => unreachable!(),
         }
     }
 }
 
-fn get_effective_address(r_m: u8) -> &'static str {
+fn get_effective_address(r_m: u8, bytes: &mut Enumerate<Bytes<BufReader<File>>>) -> String {
     match r_m {
-        0b000 => "[BX + SI]",
-        0b001 => "[BX + DI]",
-        0b010 => "[BP + SI]",
-        0b011 => "[BP + DI]",
-        0b100 => "[SI]",
-        0b101 => "[DI]",
-        0b110 => todo!("DIRECT ADDRESS"),
-        0b111 => "[BX]",
+        0b000 => String::from("[BX + SI]"),
+        0b001 => String::from("[BX + DI]"),
+        0b010 => String::from("[BP + SI]"),
+        0b011 => String::from("[BP + DI]"),
+        0b100 => String::from("[SI]"),
+        0b101 => String::from("[DI]"),
+        0b110 => {
+            let disp_lo = bytes.next().unwrap().1.unwrap() as i16;
+            let disp_hi = bytes.next().unwrap().1.unwrap() as i16;
+            let disp = (disp_hi << 8) | disp_lo;
+            format!("[{disp}]")
+        }
+        0b111 => String::from("[BX]"),
         _ => unreachable!(),
     }
 }
 
 // TODO(jmarcil): Doc comment.
-fn get_disp_registers(register_memory: u8) -> &'static str {
+fn get_disp_registers(register_memory: u8) -> String {
     match register_memory {
-        0b000 => "BX + SI",
-        0b001 => "BX + DI",
-        0b010 => "BP + SI",
-        0b011 => "BP + DI",
-        0b100 => "SI",
-        0b101 => "DI",
-        0b110 => "BP",
-        0b111 => "BX",
+        0b000 => String::from("BX + SI"),
+        0b001 => String::from("BX + DI"),
+        0b010 => String::from("BP + SI"),
+        0b011 => String::from("BP + DI"),
+        0b100 => String::from("SI"),
+        0b101 => String::from("DI"),
+        0b110 => String::from("BP"),
+        0b111 => String::from("BX"),
         _ => unreachable!(),
     }
 }
@@ -159,11 +164,11 @@ fn mov_reg_mem_to_from_reg(byte_one: u8, bytes: &mut Enumerate<Bytes<BufReader<F
     let mode: u8 = (byte_two & MOD) >> 6;
     let register: u8 = (byte_two & REG) >> 3;
     let register_memory: u8 = byte_two & R_M;
-    let register_in_reg: &str = get_reg(register, is_word);
+    let register_in_reg = get_reg(register, is_word);
 
     match mode {
         MOD_MM_NO_DISP => {
-            let effective_address: &str = get_effective_address(register_memory);
+            let effective_address = get_effective_address(register_memory, bytes);
 
             let src;
             let dst;
@@ -180,16 +185,16 @@ fn mov_reg_mem_to_from_reg(byte_one: u8, bytes: &mut Enumerate<Bytes<BufReader<F
         MOD_MM_8_BIT_DISP => {
             let disp: i8 = bytes.next().unwrap().1.unwrap() as i8;
 
-            let disp_registers: &str = get_disp_registers(register_memory);
+            let disp_registers = get_disp_registers(register_memory);
 
             let src;
             let dst;
             if destination_in_reg {
-                src = get_disp_byte(disp_registers, disp);
+                src = get_disp_byte(&disp_registers, disp);
                 dst = register_in_reg.to_owned();
             } else {
                 src = register_in_reg.to_owned();
-                dst = get_disp_byte(disp_registers, disp);
+                dst = get_disp_byte(&disp_registers, disp);
             };
 
             println!("MOV {dst}, {src}");
@@ -204,11 +209,11 @@ fn mov_reg_mem_to_from_reg(byte_one: u8, bytes: &mut Enumerate<Bytes<BufReader<F
             let src;
             let dst;
             if destination_in_reg {
-                src = get_disp_word(registers, disp);
+                src = get_disp_word(&registers, disp);
                 dst = register_in_reg.to_owned();
             } else {
                 src = register_in_reg.to_owned();
-                dst = get_disp_word(registers, disp);
+                dst = get_disp_word(&registers, disp);
             };
 
             println!("MOV {dst}, {src}");
@@ -275,12 +280,12 @@ fn mov_imm_to_r_m(byte_one: u8, bytes: &mut Enumerate<Bytes<BufReader<File>>>) {
                 let hi = bytes.next().unwrap().1.unwrap();
                 let src: i16 = (hi as i16) << 8 | lo as i16;
 
-                let dst = get_effective_address(r_m);
+                let dst = get_effective_address(r_m, bytes);
 
                 println!("MOV {dst}, WORD {src}");
             } else {
                 let src = bytes.next().unwrap().1.unwrap();
-                let dst = get_effective_address(r_m);
+                let dst = get_effective_address(r_m, bytes);
 
                 println!("MOV {dst}, BYTE {src}");
             }
@@ -288,7 +293,7 @@ fn mov_imm_to_r_m(byte_one: u8, bytes: &mut Enumerate<Bytes<BufReader<File>>>) {
         MOD_MM_8_BIT_DISP => {
             let disp = bytes.next().unwrap().1.unwrap() as i8;
             let disp_registers = get_disp_registers(r_m);
-            let dst = get_disp_byte(disp_registers, disp);
+            let dst = get_disp_byte(&disp_registers, disp);
 
             if is_word {
                 let data_lo = bytes.next().unwrap().1.unwrap();
@@ -307,7 +312,7 @@ fn mov_imm_to_r_m(byte_one: u8, bytes: &mut Enumerate<Bytes<BufReader<File>>>) {
             let disp_hi = bytes.next().unwrap().1.unwrap() as i8;
             let disp = (disp_hi as i16) << 8 | disp_lo as i16;
             let disp_registers = get_disp_registers(r_m);
-            let dst = get_disp_word(disp_registers, disp);
+            let dst = get_disp_word(&disp_registers, disp);
 
             if is_word {
                 let data_lo = bytes.next().unwrap().1.unwrap();
@@ -351,6 +356,9 @@ fn main() {
                 //  MOV - Imm to Reg
                 //--------------------------------
                 0b1011_00..=0b1011_11 => mov_imm_to_reg(byte_one, &mut bytes),
+                //--------------------------------
+                //  MOV - Imm to Reg/Mem
+                //--------------------------------
                 0b110001 => mov_imm_to_r_m(byte_one, &mut bytes),
                 _ => {
                     panic!("Unsupported OPCODE {:b}!", opcode);
